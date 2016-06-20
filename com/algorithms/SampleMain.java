5ed8c802961588aa572a9325754964b833211603
Venkata Rajendra Kumar Adepu <veadepu@veadepu.hyd.corp.google.com>
2016-06-20 14:44:48 +0530

    Best fit in BST

--- com/algorithms/SampleMain.java
+++ com/algorithms/SampleMain.java
@@ -1,18 +1,8 @@
 package com.algorithms;
 
-import com.algorithms.trees.AncestorRelated;
 import com.algorithms.trees.BSTSearching;
 import com.algorithms.trees.BinaryTreeNode;
-import com.algorithms.trees.BinaryTreeTraversals;
 import com.algorithms.trees.BuildBinarySearchTree;
-import com.algorithms.trees.CreateMirror;
-import com.algorithms.trees.DeepestNode;
-import com.algorithms.trees.MaxSumLevel;
-import com.algorithms.trees.TreeComparision;
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
 
 public class SampleMain {
 
@@ -20,13 +10,12 @@ public class SampleMain {
     //    Scanner in = new Scanner(System.in);
     //    int noOfInputs = Integer.parseInt(in.nextLine());
 
-        int[] numbers = {8, 3, 10, 1, 6, 14, 4, 7, 13};
-    
-        BinaryTreeNode root = null;
-        BinaryTreeNode node = new BinaryTreeNode(13, null, null);
+    int[] numbers = {8, 3, 10, 1, 6, 14, 4, 7, 13};
 
-    
-        root = BuildBinarySearchTree.buildBinarySearchTree(numbers);
-        System.out.println(BSTSearching.leastCommonAncestor(root,1,14) ); 
+    BinaryTreeNode root = null;
+    BinaryTreeNode node = new BinaryTreeNode(13, null, null);
+
+    root = BuildBinarySearchTree.buildBinarySearchTree(numbers);
+    System.out.println(BSTSearching.bestFit(root,110) ); 
   }
 }
