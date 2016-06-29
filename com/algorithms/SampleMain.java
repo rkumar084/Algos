60c046871fff745f719cc8b75e3682f815ce5ba5
Venkata Rajendra Kumar Adepu <veadepu@veadepu.hyd.corp.google.com>
2016-06-29 11:35:11 +0530

    some more algos on heaps

--- com/algorithms/SampleMain.java
+++ com/algorithms/SampleMain.java
@@ -1,6 +1,8 @@
 package com.algorithms;
 
+import com.algorithms.heaps.Heap;
 import com.algorithms.heaps.HeapCreation;
+import com.algorithms.heaps.HeapSearch;
 import com.algorithms.trees.BSTSearching;
 import com.algorithms.trees.BinaryTreeNode;
 import com.algorithms.trees.BuildBinarySearchTree;
@@ -11,12 +13,24 @@ public class SampleMain {
     //    Scanner in = new Scanner(System.in);
     //    int noOfInputs = Integer.parseInt(in.nextLine());
 
-    int[] numbers = {8, 3, 10, 1, 6, 14, 4, 7, 13};
-    //int[] numbers = {8, 3, 10, 1, 6, 14, 4, 7, 13, 20, 30, 25, 100, 90, 5, 35};
-    HeapCreation.buildHead(numbers);
-    HeapCreation.printHeap(numbers);
 
- 
+    int[] numbers = new int[100];
+    numbers[0] = 8; // 3, 10, 1, 6, 14 }; //, 4, 7, 13, 20, 30, 25, 100, 90, 5, 35};
+    numbers[1] = 3;
+    numbers[2] = 10;
+    numbers[3] = 1;
+    numbers[4] = 14;
+    numbers[5] = 4;
+    numbers[6] = 7;    
+    numbers[7] = 13;
+    
+    Heap heap = new Heap(numbers, 7);
+
+    heap.buildHead();
+    heap.printHeap();
+    heap.replace(5);
+    heap.printHeap();
+    
   }
   
 }
