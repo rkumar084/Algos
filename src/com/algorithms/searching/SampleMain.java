6bcc6d79a9a23fa82e6370f9d5989a34111c820b
Venkata Rajendra Kumar Adepu <veadepu@veadepu.hyd.corp.google.com>
2017-04-05 11:35:34 +0530

    Find two non repeating numbers in an array in which all other elements are repeated twice.

--- src/com/algorithms/searching/SampleMain.java
+++ src/com/algorithms/searching/SampleMain.java
@@ -2,11 +2,19 @@ package com.algorithms.searching;
 
 public class SampleMain {
 
+  //  Find 2 repeating nos in n+2 nos of range 1 to n.
+  //  public static void main(String[] args) {
+  //    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 1, 14};
+  //    Repeating.printTwoRepeatingNumbers(arr);
+  //  }
 
-  //Find 2 repeating nos in n+2 nos of range 1 to n.       
-  public static void main(String[] args) 
-  {
-      int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,1,14};
-      Repeating.printTwoRepeatingNumbers(arr);
+  //Given an array in which all numbers except two are repeated once.
+  //(i.e. we have 2n+2 numbers and n numbers are occurring twice and
+  //remaining two have occurred once). Find those two numbers in the most efficient way.
+  //Find 2 repeating nos in n+2 nos of range 1 to n.
+  public static void main(String[] args) {
+    //int arr[] = {1,1,2,2,6,6,9,9,101,101,7,3,90,90,45,45};
+    int arr[] = {1, 1, 2, 2, 3, 4};
+    Repeating.printTwoNonRepeatingNumbers(arr);
   }
 }
