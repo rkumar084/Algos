8dda59e0671b9e932b8d6624fe7b190d195e985e
Venkata Rajendra Kumar Adepu <veadepu@veadepu.hyd.corp.google.com>
2017-04-05 12:42:16 +0530

    Finding majority number in an array of elements

--- src/com/algorithms/searching/SampleMain.java
+++ src/com/algorithms/searching/SampleMain.java
@@ -12,9 +12,17 @@ public class SampleMain {
   //(i.e. we have 2n+2 numbers and n numbers are occurring twice and
   //remaining two have occurred once). Find those two numbers in the most efficient way.
   //Find 2 repeating nos in n+2 nos of range 1 to n.
+  //  public static void main(String[] args) {
+  //    //int arr[] = {1,1,2,2,6,6,9,9,101,101,7,3,90,90,45,45};
+  //    int arr[] = {1, 1, 2, 2, 3, 4};
+  //    Repeating.printTwoNonRepeatingNumbers(arr);
+  //  }
+
+  // Find majority number.
   public static void main(String[] args) {
-    //int arr[] = {1,1,2,2,6,6,9,9,101,101,7,3,90,90,45,45};
-    int arr[] = {1, 1, 2, 2, 3, 4};
-    Repeating.printTwoNonRepeatingNumbers(arr);
+    //int arr[] = {5,5,5,5,5,7,7,7,7,7,7,7};
+    // int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9};
+    int arr[] = {1, 7, 3, 7, 5, 7, 7, 9, 7, 9};
+    MajorityNumber.getMajorityNumber(arr);
   }
 }
